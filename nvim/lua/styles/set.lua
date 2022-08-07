48dbbf5790ab366b4aff29a3273ec5bf1ff61e2f
Avery Biskup <averybiskup@gmail.com>
2022-08-07 12:58:54 -0700

    Added alacritty dotfile

--- nvim/lua/styles/set.lua
+++ nvim/lua/styles/set.lua
@@ -1,26 +1,51 @@
+-- Cursor
 vim.opt.guicursor = ""
+
+-- Numbers
 vim.opt.nu = true
 vim.opt.relativenumber = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-vim.opt.wrap = false
+
+-- Search
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 vim.opt.ignorecase = true
+
+-- Misc
 vim.opt.hidden = true
 vim.opt.scrolloff = 12
-vim.opt.textwidth = 79
 vim.opt.laststatus = 3
-vim.opt.colorcolumn = "80"
 vim.opt.termguicolors = true
+
+-- Color Column
+vim.opt.colorcolumn = "80"
+vim.opt.textwidth = 79
+
+-- Splits
 vim.opt.splitright = true
 vim.opt.splitbelow = true
-vim.opt.expandtab = true
+
+-- Set Leader
 vim.g.mapleader = " "
 
+-- Indent Settings
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.smartindent = true
+vim.opt.wrap = true
+
+-- Fill Characters
+vim.opt.fillchars = {
+  vert = "│",
+  fold = "⠀",
+  eob = " ", -- suppress ~ at EndOfBuffer
+  --diff = "⣿", -- alternatives = ⣿ ░ ─ ╱
+  msgsep = "‾",
+  foldopen = "v",
+  foldsep = "|",
+  foldclose = ">",
+}
 
 
 
